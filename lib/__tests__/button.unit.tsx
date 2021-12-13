f58e135cc2fe10a205e37a657b55905d888e88e0
wuzx9905 <853529718@qq.com>
2021-12-13 18:25:32 +0800

    测试配置好了！

---
+++ lib/__tests__/button.unit.tsx
@@ -0,0 +1,14 @@
+import * as renderer from 'react-test-renderer'
+import React from 'react'
+import Button from '../button'
+
+describe('button tests',()=>{
+    it('是个 div',()=>{
+        const json = renderer.create(<Button/>).toJSON()
+        expect(json).toMatchInlineSnapshot(`
+<div>
+  按钮
+</div>
+`)
+    })
+})
