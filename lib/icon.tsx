d6e345710c17f0d15b3642338401bfb1278d95a4
wuzx9905 <853529718@qq.com>
2021-12-14 22:37:41 +0800

    简化参数 解构赋值

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -8,13 +8,13 @@ interface IconProps extends React.SVGAttributes<SVGElement> {
     name: string;
 }
 
-const Icon: React.FunctionComponent<IconProps> = (props) => {
-    const {className, ...restProps} = props;
+const Icon: React.FunctionComponent<IconProps> =
+    ({className,name, ...restProps}) => {
     return (
         <svg className={classes('doui-icon', className)}
              {...restProps}
         >
-            <use xlinkHref={`#${props.name}`}/>
+            <use xlinkHref={`#${name}`}/>
         </svg>
     );
 };
