1eabf6c29dc28f4ef996e4fb360c2c1fe9ce8479
wuzx9905 <853529718@qq.com>
2021-12-14 21:40:42 +0800

    添加 onClick 事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -5,11 +5,12 @@ import './icon.scss'
 
 interface IconProps{
     name: string;
+    onClick: React.MouseEventHandler<SVGElement>
 }
 
 const Icon:React.FunctionComponent<IconProps> = (props)=>{
     return(
-            <svg className="doui-icon">
+            <svg className="doui-icon" onClick={props.onClick}>
                 <use xlinkHref={`#${props.name}`}/>
             </svg>
     )
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Icon from './icon';
 
+const fn  = () =>{
+    console.log('fn')
+}
+
 ReactDOM.render(
     <div>
-        <Icon name="wechat"/>
-        <Icon name="alipay"/>
-        <Icon name="qq"/>
+        <Icon name="qq" onClick={fn}/>
     </div>, document.querySelector('#root'))
 
